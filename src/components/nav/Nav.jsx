e7090de634901104d20c56486a87a2bf215e082d
Nathan Jackson <nemanjamolnar50@gmail.com>
2025-09-11 01:22:28 +0200

    Implementing theme change button

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import LogoLight from "../../assets/images/logo.svg";
 import LogoDark from "../../assets/images/logo-dark.svg";
 import Sun from "../../assets/images/icon-sun.svg";
@@ -7,12 +7,28 @@ import Moon from "../../assets/images/icon-moon.svg";
 import "./nav.css";
 
 const Nav = () => {
+  const globalTheme = localStorage.getItem("data-theme");
+  const [theme, setTheme] = useState(globalTheme);
+
   return (
     <nav>
-      <img src={LogoDark} />
-      <button className="nav-change-theme-btn" id="changeThemeBtn">
-        <img src={Sun} />
-      </button>
+      <img src={theme == "dark" ? LogoDark : LogoLight} />
+      <input
+        type="checkbox"
+        id="changeThemeBtn"
+        onChange={(e) => {
+          if (e.target.checked) {
+            setTheme("light");
+            localStorage.setItem("data-theme", "light");
+          } else {
+            setTheme("dark");
+            localStorage.setItem("data-theme", "dark");
+          }
+        }}
+      />
+      <label className="nav-change-theme-btn" htmlFor="changeThemeBtn">
+        <img src={theme == "dark" ? Sun : Moon} />
+      </label>
     </nav>
   );
 };
